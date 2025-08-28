2190e56fed498eb1756d902ef04d44e14b8ab44b
Artem-Karpov32 <pikivoviw664@gmail.com>
2025-08-28 14:59:42 +0300

    изменена ошибка в файле Firestore

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCVl7_V1calMgOUx6WSqiNNPQYuH3cA7mU",
   authDomain: "ege-planner.firebaseapp.com",
@@ -15,7 +7,3 @@ const firebaseConfig = {
   appId: "1:753169005869:web:fdbe2678276c2c2efc67a4",
   measurementId: "G-6LWHYFY7LF"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
